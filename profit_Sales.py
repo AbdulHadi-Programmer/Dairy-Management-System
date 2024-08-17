d5a15c4202591d0b2ccc495a9f6655a99d8e1c62
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2024-08-17 23:04:16 +0500

    Add profit_Sales.py

---
+++ profit_Sales.py
@@ -0,0 +1,57 @@
+print("Dairy Management System")
+
+class Profit_Management:
+    def _init_(self) -> None:
+        pass
+
+    def profit_percentages(self, price):
+        # global product_specific_category
+        # try :
+        #     product_specific_category = int(input("\nSelect the Product to Caculate the Profit: \n1. Milk Shakes \n2. Cold Items (Pakola, Rooh Afza, lassi, milk bottle, cold milk) \n3. Boiled Milk \n4. Milk \n-- Enter the Option: "))
+            
+        # except Exception as e :
+        #     print(e)
+        #     self.profit_percentages(price)      
+
+        product_specific_category = int(input("\nSelect the Product to Caculate the Profit: \n1. Milk Shakes \n2. Cold Items (Pakola, Rooh Afza, lassi, milk bottle, cold milk) \n3. Boiled Milk \n4. Yogurt \n5. Milk \n-- Enter the Option: "))
+        
+        match product_specific_category:
+            case 1:  # Milk Shakes
+                milkshakes = int((price / 100 ) * 50) # 50 percent profit
+                # This function can now return the exact selling price because it add the profit percentage to that 
+                return milkshakes + price
+            
+            case 2:  # Cold Items list
+                cold_items_lst = ['Pakola Milk', 'Rooh Afza Milk', 'Cold Ice Milk', 'Lassi', 'Bottle Milk']
+                cold_item = (price / 100) * 35 # 35 is the profit percentage to calculate and this only calculate the profit not add in the price.
+                return cold_item + price
+            
+            case 3: # Boiled Milk
+                boiled_milk = (price/ 100) * 30
+                return boiled_milk + price
+            
+            case 4: # Yougurt 
+                yogurt = (price /100) * 45
+                return yogurt  + price 
+
+            case 5: # Milk
+                milk = (price/100) * 50 # last arg is profit percentage nn
+                return milk + price
+
+    def products_list(self):
+        cold_items_lst = ['Milk', 'Pakola Milk', 'Rooh Afza Milk', 'Cold Ice Milk', 'Lassi']
+        items = ['Chocolate Milkshake', 'Strawberry Milkshake', "Banana Shake", 'Bottle Milk']
+        cold_items_lst.extend(items)
+        # print(cold_items_lst)
+        print("\nThe List of Product List: \n", "-"*25)
+        for i, j in enumerate(cold_items_lst, start=1):
+            print(f"{i}. {j}")
+        print()
+
+    def add_profit_to_price(self):
+        pass
+
+p = Profit_Management()
+print(p.profit_percentages(400))
+p.profit_percentages(400)
+p.products_list()
